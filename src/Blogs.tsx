1dc1225cae19754aaf405b85933a5ce149265720
surajgupt01 <surajgupt880@gmail.com>
2025-10-06 21:06:07 +0530

    v2.1

--- src/Blogs.tsx
+++ src/Blogs.tsx
@@ -1,4 +1,4 @@
-import { blogs } from "./Blogs"
+import { blogs } from "./BlogsItems"
 
 
 export default function Blogs(){
@@ -9,7 +9,7 @@ export default function Blogs(){
               {
                 blogs.slice().reverse().map((e:string)=>(
                   
-                    <div className="bg-gray-800/50 shadow-sm backdrop:blur-md w-100 sm:w-160 h-auto p-4 m-4 text-gray-300 text-sm rounded-lg flex flex-col items-center">
+                    <div className="bg-gray-800/50 shadow-sm backdrop:blur-md w-100 sm:w-160 h-auto p-4 m-4 text-gray-300 text-sm rounded-xl flex flex-col items-center">
                          <div className="flex items-center mb-4  w-full">
                          <div className="w-10 h-10 rounded-full bg-indigo-500  font-semibold text-white flex justify-center items-center">S</div>
                          <span className="font-semibold ml-2">Suraj</span>
