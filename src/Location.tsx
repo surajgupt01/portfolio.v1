097938a444c2a717e08be4011a44b923e55cf89b
surajgupt01 <surajgupt880@gmail.com>
2026-02-14 17:03:28 +0530

    new design update

--- src/Location.tsx
+++ src/Location.tsx
@@ -6,7 +6,7 @@ export default function Location() {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="size-6"
+      className="size-4"
     >
       <path
         strokeLinecap="round"
